34ed3e331806f950b4bb49bb3741097f50502ded
Dinmukhammed <arystambekdimash005@gmail.com>
2023-04-30 00:00:04 +0600

    hello

--- Reciepts/Reciepts/settings.py
+++ Reciepts/Reciepts/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5*ewxscg!sl^5olaq)1q-#682i2&5o2ts9$kie4oqxq&e+=#bz'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
